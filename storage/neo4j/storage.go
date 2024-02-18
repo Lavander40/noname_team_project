32dc1563cbba13b177af3ebe89d88cb569b0855c
Lavander40 <a.kirill.03@mail.ru>
2024-02-18 13:52:09 +0300

    neo4j driver

--- storage/neo4j/storage.go
+++ storage/neo4j/storage.go
@@ -1,9 +1,15 @@
 package neo4j
 
-import "noname_team_project/config"
+import (
+	"context"
+	"noname_team_project/config"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
 
 type Neo4j struct {
 	conf *config.Config
+	conn neo4j.SessionWithContext
 }
 
 func New(config *config.Config) *Neo4j {
@@ -13,6 +19,13 @@ func New(config *config.Config) *Neo4j {
 }
 
 func (n *Neo4j) Open() error {
+	driver, err := neo4j.NewDriverWithContext("", neo4j.BasicAuth("user", "pass", ""))
+	if err != nil {
+		return err
+	}
+	session := driver.NewSession(context.Background(), neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+
+	n.conn = session
 	return nil
 }
 
