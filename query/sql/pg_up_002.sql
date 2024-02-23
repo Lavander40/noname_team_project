7957f59253a681234d6f371f42d03ff166627278
Lavander40 <a.kirill.03@mail.ru>
2024-02-23 18:17:36 +0300

    init scripts edit

--- query/sql/pg_up_002.sql
+++ query/sql/pg_up_002.sql
@@ -17,27 +17,62 @@ INSERT INTO groups (depart_id, spec_id) VALUES (2, 1);
 INSERT INTO groups (depart_id, spec_id) VALUES (1, 2);
 INSERT INTO groups (depart_id, spec_id) VALUES (2, 2);
 
-INSERT INTO students (full_name, group_id) VALUES ('Vladimir', 1);
-INSERT INTO students (full_name, group_id) VALUES ('Vladislav', 1);
-INSERT INTO students (full_name, group_id) VALUES ('Victor', 1);
-INSERT INTO students (full_name, group_id) VALUES ('Vyacheslav', 1);
-INSERT INTO students (full_name, group_id) VALUES ('Valeriy', 1);
-INSERT INTO students (full_name, group_id) VALUES ('Viniamin', 1);
+INSERT INTO students (full_name, group_id) VALUES ('Владимир Витальевич Теренко', 1);
+INSERT INTO students (full_name, group_id) VALUES ('Владислав Сергеевич Филлипов', 1);
+INSERT INTO students (full_name, group_id) VALUES ('Виктор Николаевич Сорокин', 1);
+INSERT INTO students (full_name, group_id) VALUES ('Вячеслав Викторович Фидзе', 1);
+INSERT INTO students (full_name, group_id) VALUES ('Евгения Григорьевна Павленко', 1);
+INSERT INTO students (full_name, group_id) VALUES ('Татьяна Валерьевна Головчак', 1);
 
-INSERT INTO students (full_name, group_id) VALUES ('Dmitriy', 2);
-INSERT INTO students (full_name, group_id) VALUES ('Denis', 2);
-INSERT INTO students (full_name, group_id) VALUES ('David', 2);
-INSERT INTO students (full_name, group_id) VALUES ('Dementiy', 2);
-INSERT INTO students (full_name, group_id) VALUES ('Sergey', 2);
-INSERT INTO students (full_name, group_id) VALUES ('Nikolay', 2);
+INSERT INTO students (full_name, group_id) VALUES ('Дмитрий Викторович Денькин', 2);
+INSERT INTO students (full_name, group_id) VALUES ('Оксана Евгеньевна Лапшина', 2);
+INSERT INTO students (full_name, group_id) VALUES ('Давид Алексеевич Клитин', 2);
+INSERT INTO students (full_name, group_id) VALUES ('Деменьтий Сергеевич Остапов', 2);
+INSERT INTO students (full_name, group_id) VALUES ('Сергей Николаевич Пегов', 2);
+INSERT INTO students (full_name, group_id) VALUES ('Екатерина Сергеевна Бубнова', 2);
 
-INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', NOW(), 1, 3, 1);
-INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', NOW(), 2, 3, 1);
-INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', NOW(), 3, 3, 1);
+INSERT INTO students (full_name, group_id) VALUES ('Валерий Леонидович Антонов', 3);
+INSERT INTO students (full_name, group_id) VALUES ('Денис Данилович Косыгин', 3);
+INSERT INTO students (full_name, group_id) VALUES ('Елена Артёмовна Жигина', 3);
+INSERT INTO students (full_name, group_id) VALUES ('Анастасия Олеговна Бугина', 3);
+INSERT INTO students (full_name, group_id) VALUES ('Николай Дмитриевич Чугин', 3);
+INSERT INTO students (full_name, group_id) VALUES ('Антон Александрович Серов', 3);
+
+INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', 2023-10-21 09:00:00, 1, 3, 1);
+INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', 2023-10-21 10:40:00, 2, 3, 1);
+INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', 2023-10-23 14:20:00, 3, 3, 1);
+INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', 2023-10-23 16:20:00, 4, 3, 1);
+INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', 2023-10-25 12:40:00, 5, 3, 1);
+INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', 2023-10-26 10:40:00, 6, 3, 1);
+INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', 2023-10-26 12:40:00, 7, 3, 1);
+INSERT INTO lessons (typing, date, lection_id, equip_id, course_id) VALUES ('лекция', 2023-10-26 14:20:00, 8, 3, 1);
 
 INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (1, 1, 1);
 INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (1, 2, 1);
 INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (1, 3, 1);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (1, 4, 2);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (1, 5, 2);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (1, 6, 2);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (1, 7, 3);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (1, 8, 3);
+
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (2, 1, 1);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (2, 2, 1);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (2, 3, 1);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (2, 4, 2);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (2, 5, 2);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (2, 6, 2);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (2, 7, 3);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (2, 8, 3);
+
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (3, 1, 1);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (3, 2, 1);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (3, 3, 1);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (3, 4, 2);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (3, 5, 2);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (3, 6, 2);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (3, 7, 3);
+INSERT INTO schedules (group_id, lesson_id, room_id) VALUES (3, 8, 3);
 
 INSERT INTO attendances (stud_id, sched_id) VALUES (1, 1);
 INSERT INTO attendances (stud_id, sched_id) VALUES (2, 1);
